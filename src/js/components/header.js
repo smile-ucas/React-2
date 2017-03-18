9d62b1491f1705f2b283e6b4a45761682f93cc3f
smile_ucas <xiaoxiaotvb@126.com>
2017-03-18 12:50:15 +0800

    first commit

---
+++ src/js/components/header.js
@@ -0,0 +1,20 @@
+/**
+ * Created by xiaoxiao on 2017/3/17.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class ComponentHeader extends React.Component{
+    render(){
+        return(
+            <header>
+            <h1>这里是头部</h1>
+
+            </header>
+
+         );
+    }
+
+
+}
